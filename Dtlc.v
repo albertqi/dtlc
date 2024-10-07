5ccbafb50ad1f18cfdc362fa51ac35f215c8633c
Albert Qi <albertqi03@gmail.com>
2024-10-07 15:50:54 -0400

    Add typing relation

--- Dtlc.v
+++ Dtlc.v
@@ -14,6 +14,22 @@ Inductive term : Type :=
   | Succ : term -> term
   | ElimNat : term -> term -> term -> term -> term.
 
+(* Term Equality *)
+Fixpoint term_beq (t1 t2 : term) : bool :=
+  match t1, t2 with
+  | Var n1, Var n2 => n1 =? n2
+  | Star, Star => true
+  | Pi ty1 t1, Pi ty2 t2 => term_beq ty1 ty2 && term_beq t1 t2
+  | Fun ty1 t1, Fun ty2 t2 => term_beq ty1 ty2 && term_beq t1 t2
+  | App t11 t12, App t21 t22 => term_beq t11 t21 && term_beq t12 t22
+  | Nat, Nat => true
+  | Zero, Zero => true
+  | Succ t1, Succ t2 => term_beq t1 t2
+  | ElimNat t1 t2 t3 t4, ElimNat t5 t6 t7 t8 =>
+      term_beq t1 t5 && term_beq t2 t6 && term_beq t3 t7 && term_beq t4 t8
+  | _, _ => false
+  end.
+
 (* De Bruijn Lifting *)
 Fixpoint lift (n : nat) (k : nat) (t : term) : term :=
   match t with
@@ -68,6 +84,51 @@ Fixpoint eval (t : term) : term :=
       end
   end.
 
+(* Typing Relation *)
+Fixpoint get_type (ctx : list term) (t : term) : option term :=
+  match t with
+  | Var n => nth_error ctx n
+  | Star => Some Star
+  | Pi ty t' =>
+      match get_type ctx ty, get_type (ty :: ctx) t' with
+      | Some Star, Some Star => Some Star
+      | _, _ => None
+      end
+  | Fun ty t' =>
+      match get_type (ty :: ctx) t' with
+      | Some ty2 =>
+          match get_type ctx ty with
+          | Some Star => Some (Pi ty ty2)
+          | _ => None
+          end
+      | None => None
+      end
+  | App t1 t2 =>
+      match get_type ctx t1, get_type ctx t2 with
+      | Some (Pi ty t'), Some ty' =>
+          if term_beq ty ty' then Some (subst 0 t2 t') else None
+      | _, _ => None
+      end
+  | Nat => Some Star
+  | Zero => Some Nat
+  | Succ t' =>
+      match get_type ctx t' with
+      | Some Nat => Some Nat
+      | _ => None
+      end
+  | ElimNat t t0 tsuc t' =>
+      match get_type ctx t, get_type ctx t0, get_type ctx tsuc, get_type ctx t' with
+      | Some (Fun Nat Star), Some (App t'' Zero), Some (Fun Nat (Fun t1 t2)), Some Nat =>
+          if negb (term_beq t'' t) then None else
+          match t1, t2 with
+          | App t1' (Var n1), App t2' (Succ (Var n2)) =>
+              if andb (term_beq t1' t2') (n1 =? n2) then Some (App t t') else None
+          | _, _ => None
+          end
+      | _, _, _, _ => None
+      end
+  end.
+
 Definition t1 := Fun Nat (App (App (Var 1) (Fun Nat (Var 3))) (Var 2)).
 
 (* fun.((1 (fun.3)) 2)[1 <- 0] = fun.((1 (fun.2)) 1) *)
@@ -90,4 +151,12 @@ Definition t3 := Fun Nat (App (Var 0) (Var 0)).
 Example eval2 :
   eval (App t3 t3) = (App t3 t3).
 
+Proof. simpl. reflexivity. Qed.
+
+(* Polymorphic Identity Function *)
+Definition t4 := Fun Star (Fun (Var 0) (Var 0)).
+
+Example get_type1 :
+  get_type [] t4 = Some (Pi Star (Pi (Var 0) (Var 0))).
+  
 Proof. simpl. reflexivity. Qed.
